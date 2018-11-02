760e1a28dd787dd8e4fa0a1bfa74ac2f452255a0
Brad Chao <brad@brad.tw>
2018-11-02 16:19:21 +0800

    v4

--- app/src/main/java/tw/brad/app/listviewtest/MainActivity.java
+++ app/src/main/java/tw/brad/app/listviewtest/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private SimpleAdapter simpleAdapter;
+    private int[] to = {R.id.item_title, R.id.item_date};
+    private String[] from = {"brad", "sakura"};
+    private LinkedList<HashMap<String,String>> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListView(){
+        data = new LinkedList<>();
 
-        simpleAdapter = new SimpleAdapter(this,null,itemLayout, from, to);
+        for (int i=0; i<100; i++) {
+            HashMap<String, String> row = new HashMap<>();
+            row.put(from[0], "事件: " + i);
+            row.put(from[1], "2018-10-23");
+            data.add(row);
+        }
+
+        simpleAdapter = new SimpleAdapter(this,
+                data,
+                R.layout.item,
+                from,
+                to);
 
         listView.setAdapter(simpleAdapter);
     }
