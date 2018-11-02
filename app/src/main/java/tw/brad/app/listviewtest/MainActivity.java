a372f4bd9860247f3f321b432d79c416d895001a
Brad Chao <brad@brad.tw>
2018-11-02 15:49:32 +0800

    v2

--- app/src/main/java/tw/brad/app/listviewtest/MainActivity.java
+++ app/src/main/java/tw/brad/app/listviewtest/MainActivity.java
@@ -2,12 +2,27 @@ package tw.brad.app.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private SimpleAdapter simpleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        initListView();
     }
+
+    private void initListView(){
+
+        simpleAdapter = new SimpleAdapter(this,null,itemLayout, from, to);
+
+        listView.setAdapter(simpleAdapter);
+    }
+
 }
